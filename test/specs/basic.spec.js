bd1005debe118fc0286e2809c3ff6f0d2cc2bb6f
rinsal <notthepeterpig@gmail.com>
2020-07-11 15:36:12 -0700

    basic test

--- test/specs/basic.spec.js
+++ test/specs/basic.spec.js
@@ -1,7 +1,7 @@
 describe('SAMPLE', () => {
   it('sample', () => {
-    browser.url('http:google.com');
+    browser.url('http://google.com');
     browser.pause(1000);
-    expect(browser.getTitle()).includes('Google');
+    expect(browser.getTitle()).eq('Google');
   });
 });
